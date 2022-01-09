6e182e2f35bbaffcf27efd250881e89d1eb09660
oguz.korkut <oguzkorkut>
2022-01-09 12:53:00 +0300

    Todo sayfası listeleme count ayarlamaları yapıldı

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/database/database.dart';
+import 'package:flutter_todo_app/models/note_model.dart';
 import 'package:flutter_todo_app/screens/add_note_screen.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,73 +13,137 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<Note>> _noteList;
+  final DateFormat _dateFormatter = DateFormat('MMM dd, yyyy');
+  DatabaseHelper _databaseHelper = DatabaseHelper.instance;
 
-  Widget _buildNote (int index){
-    return Padding(padding: EdgeInsets.symmetric(horizontal: 25.0),
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              title: Text('Note Title'),
-              subtitle: Text('Aug 8, 2022 High'),
-              trailing: Checkbox(
-                onChanged: (value){
-                  print(value);
-                },
-                activeColor: Theme.of(context).primaryColor,
-                value: true,
-              ),
+  @override
+  void initState() {
+    super.initState();
+    _updateNoteList();
+  }
+
+  _updateNoteList() {
+    _noteList = DatabaseHelper.instance.getNoteList();
+  }
+
+  Widget _buildNote(Note note) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25.0),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(
+              note.title!,
+              style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.white,
+                  decoration: note.status == 0
+                      ? TextDecoration.none
+                      : TextDecoration.lineThrough),
             ),
-            Divider(height: 5.0, color: Colors.deepPurple, thickness: 2.0,),
-          ],
-        ),
+            subtitle: Text(
+              '${_dateFormatter.format(note.date!)} - ${note.priority}',
+              style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.white,
+                  decoration: note.status == 0
+                      ? TextDecoration.none
+                      : TextDecoration.lineThrough),
+            ),
+            trailing: Checkbox(
+              onChanged: (value) {
+                note.status = value! ? 1 : 0;
+                DatabaseHelper.instance.updateNote(note);
+                _updateNoteList();
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => HomeScreen()));
+              },
+              activeColor: Theme
+                  .of(context)
+                  .primaryColor,
+              value: note.status == 1 ? true : false,
+            ),
+            onTap: () =>
+                Navigator.push(context, CupertinoPageRoute(builder: (_) =>
+                    AddNoteScreen(
+                      //updateNoteList: _updateNoteList(),
+                      //note: note
+                    ))),
+          ),
+          Divider(
+            height: 5.0,
+            color: Colors.deepPurple,
+            thickness: 2.0,
+          ),
+        ],
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).primaryColor,
-        onPressed: () {
-          Navigator.push(context, CupertinoPageRoute(builder: (_) => AddNoteScreen(),));
-        },
-        child: Icon(Icons.add),
-      ),
-      body: ListView.builder(
-        padding: EdgeInsets.symmetric(vertical: 80.0),
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-          if (index == 0) {
-            return Padding(
-                padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-                child: Column (
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'My Notes',
-                      style: TextStyle(
-                          color: Colors.deepPurple,
-                        fontSize: 40.0,
-                        fontWeight: FontWeight.bold
+        backgroundColor: Colors.blueAccent,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Theme
+              .of(context)
+              .primaryColor,
+          onPressed: () {
+            Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (_) => AddNoteScreen(),
+                ));
+          },
+          child: Icon(Icons.add),
+        ),
+        body: FutureBuilder(
+            future: _noteList,
+            builder: (context, AsyncSnapshot snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              final int completeNoteCount = snapshot.data!.where((Note note) => note.status == 1 ? true : false).toList().length;
+
+              return ListView.builder(
+                padding: EdgeInsets.symmetric(vertical: 80.0),
+                itemCount: int.parse(snapshot.data!.length.toString()) + 1,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 40.0, vertical: 20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'My Notes',
+                            style: TextStyle(
+                                color: Colors.deepPurple,
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            '$completeNoteCount of ${snapshot.data.length}',
+                            style: TextStyle(
+                                color: Colors.deepPurple,
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ],
                       ),
-                    ),
-                    SizedBox(height: 10.0,),
-                    Text(
-                      '0 - 10',
-                      style: TextStyle(
-                          color: Colors.deepPurple,
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.w600
-                      ),
-                    ),
-                  ],
-                ),
-            );
-          }
-          return _buildNote(index);
-        },
-      ),
+                    );
+                  }
+                  return _buildNote(snapshot.data![index - 1]);
+                },
+              );
+            })
     );
   }
 }
